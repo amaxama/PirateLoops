e3ed6ae6df87e4b0770d715ab8a2bd58c0facb03
amaxama <maxam.anna@gmail.com>
2018-04-10 15:39:10 -0500

    Add comments and update readme

--- PirateLoops.java
+++ PirateLoops.java
@@ -12,43 +12,64 @@ import java.util.List;
  *
  * @author annamaxam
  */
-public class PirateLoops {
+public class PirateLoops {   
+    
     public static void main(String[] args) {
-        int[] a = {1,4,3,2,2,3};
-        int[] b = {1,2,3,1,0};
+        int[] a = {1,2,1};
+        int[] b = {1,0};
         int[] c = {1,8,3,4,6,1,7,3,2};
+        int[] d = {1,4,3,2,2,3};
+        int[] e = {1,2,3,1,0};
         
-        System.out.println(answer(c));
-        System.out.println(answer(b));
+        
         System.out.println(answer(a));
+        System.out.println(answer(b));
+        System.out.println(answer(c));
+        System.out.println(answer(d));
+        System.out.println(answer(e));
     }
     
-    
-    
     public static int answer(int[] numbers) {
-        int numberOfPirates = 1;
-        List<Integer> nextIndices = new ArrayList<>();
-        List<Integer> list = new ArrayList<>();
+        // Counter to keep track of number of pirates in the loop
+        int numberOfPirates = 1; 
         
-        int m=0;
-        boolean isRepeat = false;
+        // List of barstools (indices) you stop at (by number)
+        List<Integer> nextIndices = new ArrayList<>(); 
+        
+        // List of tattooed ID numbers of pirates you talk to 
+        List<Integer> idNumbers = new ArrayList<>(); 
+        
+        // Barstool (index) number of the pirate you will talk to next - starts at 0
+        int m=0; 
+        
+        // Track whether you've started the loop
+        boolean isRepeat = false; 
         
         do {
-            list.add(numbers[m]);
-            nextIndices.add(m);
+            // Add barstool (index) # of the next pirate you will talk to 
+            nextIndices.add(m); 
+            // Go to that barstool and add ID # of that pirate to the list
+            idNumbers.add(numbers[m]); 
             for (int i = 0; i <nextIndices.size(); i++) {
-                if (numbers[m] == nextIndices.get(i)) {
-                    isRepeat = true;
+                // If the ID # of the current pirate matches a number in the list of barstools you've been to, you have been to this pirate before and have started a loop.
+                if (numbers[m] == nextIndices.get(i)) { 
+                    // Change the repeat tracker to true and break out of the for loop
+                    isRepeat = true; 
+                    break; 
                 }
             }
-            m = numbers[m];
+            // Move to the next barstool based on the ID # of the pirate you're currently talking to
+            m = numbers[m]; 
            
-        } while (! isRepeat);
+        } while (! isRepeat); 
+
         
-        System.out.println(list);
-        int beginningOfLoop = list.get(list.size()-1);
-        for(int i = list.size()-2;i>=0;i--) {
-            if (beginningOfLoop != list.get(i)) {
+//      Beginning of the loop is the last ID # that got added to the idNumbers list
+        int beginningOfLoop = idNumbers.get(idNumbers.size()-1);
+        
+//      Go backwards through the idNumbers list, adding the number of pirates until an ID # matches the last number that got added to the list
+        for(int i = idNumbers.size()-2;i>=0;i--) {
+            if (beginningOfLoop != idNumbers.get(i)) {
                 numberOfPirates+=1;
             } else {
                 return numberOfPirates;
